fa687a0f5aba8829986769da5d97a37f7f87104f
BlakeStover <b10stover@gmail.com>
2026-03-25 11:43:01 -0500

    added register and login routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 require('./config/db');
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello!' });
